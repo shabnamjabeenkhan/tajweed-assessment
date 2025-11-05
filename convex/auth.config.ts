bf628a541f2e78feffdadacfdf59574f1c50b6c4
Shabnam Khan <shabnamkhan@Shabnams-MacBook-Pro.local>
2025-11-05 05:49:34 +0000

    auth.config.ts domain url changed

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: process.env.VITE_CLERK_FRONTEND_API_URL,
+        domain: "https://clerk.tajweedsimplified.com",
         applicationID: "convex",
       },
     ]
